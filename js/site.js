9eb3bdce7945627c29f76ca9b62fe13150a135cd
Zach Inscho <zach@drxlr.com>
2016-05-06 10:12:05 -0400

    swap sass

---
+++ js/site.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(function() {
+    	$(".rslides-plot").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 2300,          // Integer: Time between slide transitions, in milliseconds
+		});
+		$(".rslides-hailo").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 3600,          // Integer: Time between slide transitions, in milliseconds
+		});
+
+		$(".rslides-barca").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 1500,          // Integer: Time between slide transitions, in milliseconds
+		});
+		$(".rslides-london").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 1800,          // Integer: Time between slide transitions, in milliseconds
+		});
+		$(".rslides-copenhagen").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 1300,          // Integer: Time between slide transitions, in milliseconds
+		});
+		$(".rslides-india2013").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 300,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 1700,          // Integer: Time between slide transitions, in milliseconds
+		});
+  	});
+});
